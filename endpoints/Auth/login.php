1d5868e4862b19a0ed85b0175772d4c7d67271b8
farhath6649 <134671763+farhath6649@users.noreply.github.com>
2023-06-13 13:45:35 -0400

    Update Login Page
    Updated Login Page

--- endpoints/Auth/login.php
+++ endpoints/Auth/login.php
@@ -1,17 +1,31 @@
 <?php
+//Include the required files
 require_once('../includes/config.php');
-require_once('../includes/keyword.php');
+require_once('../includes/keyboard.php');
+
+//create an array to store responses
 $response = array();
+
+//check the request method
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    //retrieve the email and password from the POST data 
     $email = isset($_POST['email']) ? $_POST['email'] : '';
     $password = isset($_POST['password']) ? $_POST['password'] : '';
+
+//check both email and password are not empty
 if(!empty($email) && !empty($password)){
     try {
+        //build the query to fetch the user data 
         $query = "SELECT * FROM user WHERE email = '{$email}' AND password = '{$password}'";
+
+        //execute the query
         $result =  $conn->query($query);
         if ($result) {
             $row = mysqli_fetch_assoc($result);
             // while ($row = mysqli_fetch_assoc($result))
+
+            //check whether email and password matches with the retrieved data
             if($row['email'] == $email && $password == $row['password'])
             {
                 $response['id'] = $row['id'];
@@ -27,7 +41,9 @@ if(!empty($email) && !empty($password)){
             }
            
         }
-    } catch (Exception $e) {
+    }
+    //display error messages 
+    catch (Exception $e) {
         $mainResponse[$status_keyword] = false;
         $mainResponse[$message_keyword] = $internal_message_keyword;
     }
@@ -46,8 +62,10 @@ else{
     $mainResponse[$message_keyword] = $wrongRequest_message_keyword;
 }
 
+//set the header content type as JSON
 header("CONTENT-TYPE:JSON");
 
+//encode the main response array as JSON
 echo json_encode($mainResponse, JSON_PRETTY_PRINT);
 ?>
 
