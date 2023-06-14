f5b2ae03b337443e6dc42bdbb6603d1e12801f5c
abhaygajeepara <56512543+Abhaygajeepara@users.noreply.github.com>
2023-06-14 09:50:16 -0400

    update

--- endpoints/User/getUser.php
+++ endpoints/User/getUser.php
@@ -3,23 +3,28 @@ require_once('../includes/config.php');
 require_once('../includes/keyword.php');
 $response = array();
 if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-    $userId = $_GET['userId'];
-    if(!empty($userId) && is_numeric($userId)){
-        
-try {
-
    
+    $userId =  isset($_GET['userId']) ? $_GET['userId'] : '';
+    if(!empty($userId) && is_numeric($userId)){
+try {
     $query = "select * from user where id = $userId";
     $result =  $conn->query($query);
     if ($result) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $response['id'] = $row['id'];
-            $response['email'] = $row['email'];
-            $response['username'] = $row['username'];
+        if (mysqli_num_rows($result)>0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $response['id'] = $row['id'];
+                $response['email'] = $row['email'];
+                $response['username'] = $row['username'];
+            }
+            $mainResponse[$data_keyword] = $response;
+            $mainResponse[$status_keyword] = true;
+            $mainResponse[$message_keyword] = $successfulMessage_keyword;
+        }else{
+         
+            $mainResponse[$status_keyword] = false;
+            $mainResponse[$message_keyword] = $dataNotfound_message_keyword;
         }
-        $mainResponse[$data_keyword] = $response;
-        $mainResponse[$status_keyword] = true;
-        $mainResponse[$message_keyword] = $successfulMessage_keyword;
+       
     }
 } catch (Exception $e) {
     $mainResponse['status'] = false;
